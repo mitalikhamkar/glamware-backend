ffe797ed5e9bcab09a971fa64a46227def97d70b
mitalikhamkar <mitalikhamkar200%@gmail.com>
2026-02-25 11:25:42 +0530

    fixed

--- src/models/Wardrobe.js
+++ src/models/Wardrobe.js
@@ -1,25 +1,5 @@
-import express from "express";
 import mongoose from "mongoose";
-import {
-  createWardrobe,
-  getAllWardrobe
-} from "../controllers/wardrobeController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-import upload from "../middleware/multer.js";
-const router = express.Router();
 
-router.post(
-  "/",
-  authMiddleware,
-  upload.single("image"),
-  createWardrobe
-);
-
-router.get(
-  "/",
-  authMiddleware,
-  getAllWardrobe
-);
 const wardrobeSchema = new mongoose.Schema(
   {
     user: {
@@ -27,17 +7,34 @@ const wardrobeSchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
-    meshUri: String,
-    textureUri: String,
-    color: String,
-    materialMode: String,
-    clothingType: String,
-    category: String,
-    size: String,
-    occasion: String,
+    meshUri: {
+      type: String,
+    },
+    textureUri: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    materialMode: {
+      type: String,
+    },
+    clothingType: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    size: {
+      type: String,
+    },
+    occasion: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
 
 const Wardrobe = mongoose.model("Wardrobe", wardrobeSchema);
-export default router;
+
+export default Wardrobe;
